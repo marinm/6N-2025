b3998375fd17601e170f7d31654cc9e1c5840f41
Marin <marinmema@live.ca>
2025-02-08 12:07:43 -0500

    display as table

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -7,12 +7,30 @@ type Props = {
 export default function PlayerList({ players }: Props) {
     return (
         <div className="scroller">
-            {players.map((p) => (
-                <div>
-                    {p.nation} - {p.name} [{p.position}] ({p.age}, {p.height},{" "}
-                    {p.weightPounds} lbs)
-                </div>
-            ))}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nation</th>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Age</th>
+                        <th>Height</th>
+                        <th>Weight (lbs)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {players.map((p) => (
+                        <tr>
+                            <td>{p.nation}</td>
+                            <td>{p.name}</td>
+                            <td>{p.position}</td>
+                            <td>{p.age}</td>
+                            <td>{p.height}</td>
+                            <td>{p.weightPounds}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
