d80ff6957b9b72fcce169e8ca5679524a24f6405
Marin <marinmema@live.ca>
2025-02-08 10:37:40 -0500

    toggle selected country

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+    const [nation, setNation] = useState<string>("france");
+
     return (
         <div>
-            Guiness Men's Six Nations 2025
+            <div className="flag-icons">
+                <button
+                    className="flag-icon"
+                    onClick={() => setNation("italy")}
+                >
+                    🇮🇹
+                </button>
+                <button
+                    className="flag-icon"
+                    onClick={() => setNation("wales")}
+                >
+                    🏴󠁧󠁢󠁷󠁬󠁳󠁿
+                </button>
+                <button
+                    className="flag-icon"
+                    onClick={() => setNation("france")}
+                >
+                    🇫🇷
+                </button>
+                <button
+                    className="flag-icon"
+                    onClick={() => setNation("england")}
+                >
+                    🏴󠁧󠁢󠁥󠁮󠁧󠁿
+                </button>
+                <button
+                    className="flag-icon"
+                    onClick={() => setNation("scotland")}
+                >
+                    🏴󠁧󠁢󠁳󠁣󠁴󠁿
+                </button>
+                <button
+                    className="flag-icon"
+                    onClick={() => setNation("ireland")}
+                >
+                    🇮🇪
+                </button>
+            </div>
+            <div className="scroller">
+                {nation === "italy" ? "Italy players" : ""}
+                {nation === "wales" ? "Wales players" : ""}
+                {nation === "france" ? "France players" : ""}
+                {nation === "england" ? "England players" : ""}
+                {nation === "scotland" ? "Scotland players" : ""}
+                {nation === "ireland" ? "Ireland players" : ""}
+            </div>
         </div>
     );
 }
